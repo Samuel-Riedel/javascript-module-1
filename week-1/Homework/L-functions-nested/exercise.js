af8af90f6c907ecd27fe5d1b828cdf9262001610
Wolfiestein <samuel.riedel@outlook.com>
2023-05-10 06:59:00 +0200

    HW_Done!

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,29 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+let mentor1 = "Daniel";
+let mentor2 = "Irina";
+let mentor3 = "Mimi";
+let mentor4 = "Rob";
+let mentor5 = "Yohannes";
+
+// write a function that logs a string to uppercase (with the name Daniel it should log HELLO DANIEL)
+function upperString(Hola) {
+    return "Hello my name is : " + Hola.toUpperCase()
+}
+const upperCaseName = upperString("Hola")
+
+
+function newFunction(Hola="Saludo", nombre="user"){
+    return upperString(Hola) + nombre
+}
+
+console.log(newFunction("Hola ",mentor1))
+console.log(newFunction("Hola ",mentor2))
+console.log(newFunction("Hola ",mentor3))
+console.log(newFunction("Hola ",mentor4))
+console.log(newFunction("Hola ",mentor5))
+/*
+greetingMentors(mentor1)
+greetingMentors(mentor2)
+greetingMentors(mentor3)
+greetingMentors(mentor4)
+greetingMentors(mentor5)
+*/
